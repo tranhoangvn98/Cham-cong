803ca4dd7186a9986448688db6554d7291a979a1
Tran Duc Hoang <152060572+tranduchoang98@users.noreply.github.com>
2026-09-19 22:03:31 +0700

    ignore script chan doan tam

--- __chuan_doan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Xem ten_luu_docx cua ban nhap moi nhat va cac tep thuc su trong /du_lieu/ho_so.
-docker exec chamcong_test_db psql -q -U chamcong -d chamcong_test_ai -tAc \
-  "select ma, ten_luu_docx from thong_bao_nhap_ai where ma = 'TBN-000075'"
-echo '--- toan bo /du_lieu/ho_so ---'
-docker exec chamcong_test_maychu sh -c 'find /du_lieu/ho_so -type f 2>/dev/null | head -20'
-echo '--- log worker gan day ---'
-docker logs chamcong_test_maychu --since 40m 2>&1 | grep -iE 'deepseek|soan|gate|docx|LoiDeepSeek|loi mang' | tail -12
